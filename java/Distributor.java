fbb032d9b270ee0c7a0fb2e6c82e432bb800265e
Franco Reyes <freyes1988@gmail.com>
2024-02-20 21:57:46 -0600

    Add prompts to Java file

--- java/Distributor.java
+++ java/Distributor.java
@@ -1,3 +1,21 @@
+/*
+Dependencies: Sqlite
+
+The exercise below uses the following schema
+
+CREATE TABLE retailers (id int, name text, liquor_license text, active boolean);
+CREATE TABLE retailer_users (id int, retailer_id int, user_id int, role text);
+CREATE TABLE distributors (id int, name text);
+CREATE TABLE retailer_distributors (id int, retailer_id int, distributor_id int, account_number text);
+CREATE TABLE users (id int, first_name text, last_name text);
+
+Refer to README.md for the contents of the database
+
+The following program will run but has areas for improvement.
+The output of index is expected to be a list of distributors for an active retailer the user belongs to.
+The output of index is expected to be in a JSON format.
+*/
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -48,6 +66,9 @@ public class Distributor {
 
     public static void main(String[] args) {
         Distributor distributor = new Distributor();
+
+
+        // Have the output be in JSON format, Do we have relevant data for the endpoint?
         ResultSet result = distributor.index("1");
         try {
             while (result.next()) {
@@ -56,5 +77,49 @@ public class Distributor {
         } catch (Exception e) {
             e.printStackTrace();
         }
+
+        // What happens if the input is not an id?
+        /*
+        result = distributor.index("user_1");
+        try {
+            while (result.next()) {
+                System.out.println(result.getString("retailer_name"));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        */
+
+        // How do we protect against malicious input?
+        /*
+        result = distributor.index("1; INSERT INTO users VALUES (100, 'New', 'User');--");
+        try {
+            while (result.next()) {
+                System.out.println(result.getString("retailer_name"));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        */
+
+        // What if we only want user's with an admin role to view this?
+        /*
+        result = distributor.index("1");
+        try {
+            while (result.next()) {
+                System.out.println(result.getString("retailer_name"));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        result = distributor.index("2");
+        try {
+            while (result.next()) {
+                System.out.println(result.getString("retailer_name"));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        */
     }
 }
