6d1724867272f4896d2da286b6907d1cad4f7369
Franco Reyes <freyes1988@gmail.com>
2024-02-21 15:16:00 -0600

    Corrected build.gradle and moved it to Kotlin

--- java/build.gradle.kts
+++ java/build.gradle.kts
@@ -4,10 +4,13 @@
  * This is a general purpose Gradle build.
  * To learn more about Gradle by exploring our Samples at https://docs.gradle.org/8.6/samples
  */
+plugins {
+    java
+}
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    implementation 'org.xerial:sqlite-jdbc:3.34.0'
+    implementation("org.xerial:sqlite-jdbc:3.34.0")
 }
